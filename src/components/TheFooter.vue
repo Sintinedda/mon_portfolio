9f0da2647a8bf3b6436d455b7b9b3dbeacb741c6
Jeremy Aboudan <aboudan.jeremy@gmail.com>
2024-03-13 16:42:03 +0100

    add-footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,32 @@
+
+<template>
+   <div class="logo">
+      <a href="https://www.facebook.com/Jerem.Sinti"><img src="../assets/img/logo-facebook.jpg" alt="Logo de Facebook"></a>
+      <a href="https://github.com/Sintinedda"><img src="../assets/img/logo-github.jpg" alt="Logo de Github"></a>
+      <a href="https://www.linkedin.com/in/jeremy-aboudan-66668a2a9/"><img src="../assets/img/logo-linkedin.png" alt="Logo de Linkedin"></a>
+      <a href="https://twitter.com/Sintine2da"><img src="../assets/img/logo-x.png" alt="Logo de X"></a>
+   </div>
+   <p>Dernière mise à jour le <time datetime="2024-03-13">13 Mars 2024</time>.</p>
+</template>
+
+<style scoped>
+   .logo{
+      display: flex;
+      column-gap: 20px;
+      justify-content: center;
+      padding-top: 2rem;
+   }
+
+   .logo img{
+      width: 80px;
+      border-radius: 12%;
+   }
+
+   p{
+      text-align: center;
+      padding: 1rem;
+      font-style: italic;
+   }
+</style>
+
+
