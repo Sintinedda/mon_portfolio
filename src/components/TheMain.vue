3c55b8436f63c315b7d63640d80e28ecea63fc1f
Jeremy Aboudan <aboudan.jeremy@gmail.com>
2024-03-18 17:42:32 +0100

    add-graphics

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -3,16 +3,16 @@
 </script>
 
 <template>
-    <div id="presentation">
+    <div id="presentation" class="menu-item">
         <h1>Présentation.</h1>
         <h2>Jérémy Aboudan</h2>
         <p>Je suis un homme de 33 ans. Après avoir travaillé dans différents secteurs d'activité tels que l'hôtellerie-restauration, le batîment et la vente je cherche aujourd'hui à me stabiliser et à choisir un secteur me corresppondant plus. Ayant toujours été attiré par le monde de l'informatique et des jeux vidéos, le métier de développeur me paraît être une option idéale.</p>
     </div>
-    <div id="creation">
+    <div id="creation" class="menu-item">
         <h1>Mes créations.</h1>
         <div id="modal"><TheModal /></div>
     </div>
-    <div id="contact">
+    <div id="contact" class="menu-item">
         <h1>Me contacter.</h1>
         <form action="">
             <section class="name">
